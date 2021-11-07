523ca15ba55e8cc4362e33b16b319138af278aa6
Robin <hello@robinblomberg.se>
2021-11-07 11:09:09 +0100

    Rewrite module as extension of Zod object

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,82 @@
-import { deepStrictEqual } from 'assert';
-import { $string } from '.';
+import { doesNotThrow, throws } from 'assert';
+import { $ } from '.';
 
-deepStrictEqual($string.safeParse('hello'), {
-  data: 'hello',
-  success: true,
-});
+// cuid:
+{
+  doesNotThrow(() => $.cuid().parse('cjld2cjxh0000qzrmn831i7rn'));
+  throws(() => $.cuid().parse('jld2cjxh0000qzrmn831i7rn'));
+}
+
+// email:
+{
+  doesNotThrow(() => $.email().parse('foo@example.com'));
+  throws(() => $.email().parse('fooexample.com'));
+}
+
+// int:
+{
+  doesNotThrow(() => $.int().parse(42));
+  throws(() => $.int().parse(42.1));
+}
+
+// nestring:
+{
+  doesNotThrow(() => $.nestring().parse('a'));
+  throws(() => $.nestring().parse(''));
+}
+
+// object:
+{
+  doesNotThrow(() => $.object({ foo: $.string() }).parse({ foo: '' }));
+  throws(() => $.object({}).parse({ foo: '' }));
+}
+
+// port:
+{
+  doesNotThrow(() => $.port().parse('0'));
+  doesNotThrow(() => $.port().parse('9'));
+  doesNotThrow(() => $.port().parse('10'));
+  doesNotThrow(() => $.port().parse('99'));
+  doesNotThrow(() => $.port().parse('100'));
+  doesNotThrow(() => $.port().parse('999'));
+  doesNotThrow(() => $.port().parse('1000'));
+  doesNotThrow(() => $.port().parse('9999'));
+  doesNotThrow(() => $.port().parse('10000'));
+  doesNotThrow(() => $.port().parse('59999'));
+  doesNotThrow(() => $.port().parse('60000'));
+  doesNotThrow(() => $.port().parse('64999'));
+  doesNotThrow(() => $.port().parse('65000'));
+  doesNotThrow(() => $.port().parse('65499'));
+  doesNotThrow(() => $.port().parse('65500'));
+  doesNotThrow(() => $.port().parse('65529'));
+  doesNotThrow(() => $.port().parse('65530'));
+  doesNotThrow(() => $.port().parse('65535'));
+  throws(() => $.port().parse('00'));
+  throws(() => $.port().parse('65536'));
+  throws(() => $.port().parse('99999'));
+  throws(() => $.port().parse('100000'));
+}
+
+// regex:
+{
+  doesNotThrow(() => $.regex(/^foo/).parse('foobar'));
+  throws(() => $.regex(/^foo/).parse('barfoo'));
+}
+
+// uint:
+{
+  doesNotThrow(() => $.uint().parse(0));
+  throws(() => $.uint().parse(-1));
+}
+
+// url:
+{
+  doesNotThrow(() => $.url().parse('http://www.example.com'));
+  throws(() => $.url().parse('http//www.example.com'));
+}
+
+// uuid:
+{
+  doesNotThrow(() => $.uuid().parse('123e4567-e89b-12d3-a456-426614174000'));
+  throws(() => $.uuid().parse('123e4567e89b-12d3-a456-426614174000'));
+}
--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,41 @@
 import { z } from 'zod';
 
+const PORT_REGEXP =
+  /^([0-9]|[1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$/;
+
 export type { infer as $infer } from 'zod';
 export { z };
 
-export const $and = z.intersection;
-
-export const $array = z.array;
-
-export const $bigint = z.bigint();
-
-export const $boolean = z.boolean();
-
-export const $cuid = z.string().cuid();
-
-export const $date = z.date();
-
-export const $email = z.string().email();
-
-export const $enum = z.enum;
-
-export const $function = z.function();
-
-export const $instanceof = z.instanceof;
-
-export const $int = z.number().int();
-
-export const $literal = z.literal;
-
-export const $map = z.map;
-
-export const $nestring = z.string().nonempty();
-
-export const $null = z.null();
-
-export const $regex = (regex: RegExp) => {
-  return z.string().regex(regex);
+export const $ = {
+  ...z,
+  cuid() {
+    return z.string().cuid();
+  },
+  email() {
+    return z.string().email();
+  },
+  int() {
+    return z.number().int();
+  },
+  nestring() {
+    return z.string().nonempty();
+  },
+  object<T extends z.ZodRawShape>(schema: T) {
+    return z.object(schema).strict();
+  },
+  port() {
+    return z.string().regex(PORT_REGEXP);
+  },
+  regex(regex: RegExp) {
+    return z.string().regex(regex);
+  },
+  uint() {
+    return z.number().int().nonnegative();
+  },
+  url() {
+    return z.string().url();
+  },
+  uuid() {
+    return z.string().uuid();
+  },
 };
-
-export const $port = $regex(/[1-9][0-9]{1,4}/);
-
-export const $promise = z.promise;
-
-export const $object = <T extends z.ZodRawShape>(schema: T) => {
-  return z.object(schema).strict();
-};
-
-export const $or = z.union;
-
-export const $record = z.record;
-
-export const $set = z.set;
-
-export const $string = z.string();
-
-export const $tuple = z.tuple;
-
-export const $uint = z.number().int().nonnegative();
-
-export const $undefined = z.undefined();
-
-export const $unknown = z.unknown();
-
-export const $uuid = z.string().uuid();
-
-export const $url = z.string().url();
